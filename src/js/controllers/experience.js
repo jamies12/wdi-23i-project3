523fc890dfd4ca717b74cfa397e06bf1e4222578
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-11-16 17:07:51 +0000

    Added sessions folder within templates with html files for breathing,sound, and eliza based 'experiences/sessions'. Also added states in app.js for these as well as an experience controller with four necessary controller functions.

---
+++ src/js/controllers/experience.js
@@ -0,0 +1,26 @@
+angular.module('moodApp')
+  .controller('SoundScapeController', SoundScapeController)
+  .controller('InstrumentExperienceController', InstrumentExperienceController)
+  .controller('BreathingExerciseController', BreathingExerciseController)
+  .controller('ElizaChatBotController', ElizaChatBotController);
+
+
+SoundScapeController.$inject = [];
+function SoundScapeController() {
+
+}
+
+InstrumentExperienceController.$inject = [];
+function InstrumentExperienceController() {
+
+}
+
+BreathingExerciseController.$inject = [];
+function BreathingExerciseController() {
+
+}
+
+ElizaChatBotController.$inject = [];
+function ElizaChatBotController() {
+  
+}
