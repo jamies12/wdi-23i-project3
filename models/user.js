03d5431430d0ecaacd88046ce63a5e04e4c68ad5
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-11-16 14:16:54 +0000

    Referenced initialUserData inside userSchema as initialData. Should work but remains to be tested

--- models/user.js
+++ models/user.js
@@ -1,9 +1,12 @@
 const mongoose  = require('mongoose');
 const bcrypt    = require('bcrypt');
 
+
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
+  initialData: [
+    { type: mongoose.Schema.ObjectId, ref: 'InitialData'} ],
   passwordHash: { type: String }
 });
 
