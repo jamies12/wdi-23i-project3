bbe0e2116a0cfe22cae948dc6974666e0a93c5ad
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-11-16 14:00:43 +0000

    Created basic InitialDataSchema, for information to be obtained from user after first login.

---
+++ models/initialUserData.js
@@ -0,0 +1,25 @@
+const mongoose  = require('mongoose');
+
+const initialDataSchema = new mongoose.Schema({
+  Gender: { type: String, required: true },
+  Age: { type: String, required: true },
+  WhatBringsYouHere: { type: String, required: true },
+  Worries: { type: String, required: true },
+  Happies: { type: String, required: true },
+  Interests: { type: String, required: true },
+  FavAnimal: { type: String, required: true },
+  HopeToGain: { type: String, required: true }
+});
+
+
+module.exports = mongoose.model('InitialData', initialDataSchema);
+
+
+// Gender?
+// Age?
+// Favourite animal?
+// What brings you here?
+// What aspects of life worry you?
+// What aspects of like make you happy?
+// What are your interests/hobbies?
+// What do you hope to gain from using this app?
