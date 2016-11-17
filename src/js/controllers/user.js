69e6e9d49a1933cc7b0497050bea7585d678dda9
Sarah Penney <sarahpenneyuk@gmail.com>
2016-11-17 16:30:11 +0000

    fixed conflict

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -1,6 +1,7 @@
 angular.module('moodApp')
-  .controller('UserFormController', UserFormController)
-  .controller('UserDataController', UserDataController);
+ .controller('UserFormController', UserFormController)
+ .controller('UserDataController', UserDataController)
+ .controller('MoodCarouselController', MoodCarouselController);
 
 UserFormController.$inject = ['$auth', 'User', '$state'];
 function UserFormController($auth, User, $state) {
@@ -9,8 +10,10 @@ function UserFormController($auth, User, $state) {
   userForm.user = User.get({ id: $auth.getPayload()._id });
 
   function submit() {
+   // console.log('pre update', userForm.user);
     userForm.user.$update(() => {
-      console.log('your data', userForm.user);
+
+      console.log('post update your data', userForm.user);
       $state.go('moodIndex');
     });
   }
@@ -25,4 +28,38 @@ function UserDataController ($auth, User ) {
 
   userData.user = User.get({ id: $auth.getPayload()._id });
 
+}
+
+MoodCarouselController.$inject = ['$auth', 'User', '$state'];
+function MoodCarouselController($auth, User, $state) {
+  const moodCarousel = this;
+
+  moodCarousel.user = User.get({ id: $auth.getPayload()._id });
+
+  function moodSelect(mood) {
+    console.log('clicked: ', mood);
+
+
+    moodCarousel.user.mood = mood;
+    console.log(moodCarousel.user);
+
+
+    const updateResult = User.update({id: $auth.getPayload()._id}, moodCarousel.user);
+    console.log('updateResult:', updateResult);
+
+
+   // moodCarousel.user.$update(() => {
+   //   // TODO: find out what this first parameter is used for -
+   //   // check documentation for $update()
+   //   console.log('moodCarousel.user.$update: find out why we got here!!');
+   // }, () => {
+   //   console.log('your data', moodCarousel.user);
+    $state.go('userData');
+   // }, () => {
+   //   console.log('moodCarousel.user.$update: error(?): arguments:', arguments);
+   // });
+
+
+  }
+  moodCarousel.moodSelect = moodSelect;
 }
