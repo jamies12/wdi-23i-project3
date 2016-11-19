e23af2a47360874cc1b994c33ec985a2ede27d35
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-11-19 15:52:30 +0000

    tweaked ripple timings

--- models/instrument.js
+++
@@ -1,8 +0,0 @@
-const mongoose  = require('mongoose');
-
-const instrumentSchema = new mongoose.Schema({
-  number: { type: String },
-  source: { type: String}
-});
-
-module.exports = mongoose.model('Instruments', instrumentSchema);
